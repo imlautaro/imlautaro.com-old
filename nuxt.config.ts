1e1abaec4ac0b5e8ad86b9b03420cae29221e4b4
Lautaro Pereyra <dev.lautaropereyra@gmail.com>
2021-09-21 20:44:13 -0300

    add google-fonts module for nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,18 @@
 import { NuxtConfig } from '@nuxt/types'
 
 const config: NuxtConfig = {
-	buildModules: ['@nuxt/typescript-build', 'nuxt-windicss'],
+	buildModules: [
+		'@nuxt/typescript-build',
+		'nuxt-windicss',
+		'@nuxtjs/google-fonts',
+	],
 	components: true,
+	googleFonts: {
+		display: 'swap',
+		families: {
+			'Titillium+Web': [400, 600, 700],
+		},
+	},
 	modules: ['@nuxtjs/robots', '@nuxt/content'],
 	srcDir: 'src',
 	target: 'static',
---
+++ windi.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'windicss/helpers'
+
+const config = defineConfig({
+	theme: {
+		fontFamily: {
+			sans: ['"Titillium Web"', 'sans-serif'],
+		},
+	},
+})
+
+export default config
