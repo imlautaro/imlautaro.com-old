d5d33d827023d0507d98347efc66ce7633c20010
Lautaro Pereyra <dev.lautaropereyra@gmail.com>
2021-09-21 20:36:53 -0300

    add robots module for nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ import { NuxtConfig } from '@nuxt/types'
 const config: NuxtConfig = {
 	buildModules: ['@nuxt/typescript-build'],
 	components: true,
+	modules: ['@nuxtjs/robots'],
 	srcDir: 'src',
 	target: 'static',
 }
