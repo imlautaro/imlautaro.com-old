6bde58a69c824eb0ad79cb8f8fbe352789299e22
Lautaro Pereyra <dev.lautaropereyra@gmail.com>
2021-09-22 09:46:18 -0300

    add google-tag-manager module for nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,7 +20,10 @@ const config: NuxtConfig = {
 			'Titillium+Web': [400, 600, 700],
 		},
 	},
-	modules: ['@nuxtjs/robots', '@nuxt/content'],
+	gtm: {
+		id: 'GTM-575736G',
+	},
+	modules: ['@nuxtjs/robots', '@nuxt/content', '@nuxtjs/gtm'],
 	pwa: {
 		manifest: {
 			lang: 'en',
