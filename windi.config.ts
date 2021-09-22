727e96bf3b2c7130b99e0226bf4f65dbbe4fe80d
Lautaro Pereyra <dev.lautaropereyra@gmail.com>
2021-09-21 21:15:21 -0300

    add default styles for containers

--- windi.config.ts
+++ windi.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from 'windicss/helpers'
 
 const config = defineConfig({
 	theme: {
+		container: {
+			center: true,
+			padding: '1rem',
+		},
 		fontFamily: {
 			sans: ['"Titillium Web"', 'sans-serif'],
 		},
