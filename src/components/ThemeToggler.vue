820bef8401d91dff2c7c562af8bbef6156d3fbe7
Lautaro Pereyra <hi@imlautaro.com>
2021-10-21 09:47:44 -0300

    refactor: update theme-toggler to avoid warnings

--- src/components/ThemeToggler.vue
+++ src/components/ThemeToggler.vue
@@ -1,26 +1,23 @@
 <template>
-	<button @click="toggleTheme" class="icon-btn">
+	<s-button
+		@click="
+			$colorMode.preference =
+				$colorMode.value == 'dark' ? 'light' : 'dark'
+		"
+		icon
+		size="sm"
+		variant="text"
+	>
 		<ph-moon-bold
 			v-if="$colorMode.value === 'dark'"
 			height="24px"
 			width="24px"
 		/>
 		<ph-sun-bold v-else height="24px" width="24px" />
-	</button>
+	</s-button>
 </template>
 
 <script setup>
-import { useContext } from '@nuxtjs/composition-api'
 import PhMoonBold from '~icons/ph/moon-bold'
 import PhSunBold from '~icons/ph/sun-bold'
-
-const { $colorMode } = useContext()
-
-const toggleTheme = () => {
-	if ($colorMode.value === 'dark') {
-		$colorMode.preference = 'light'
-	} else {
-		$colorMode.preference = 'dark'
-	}
-}
 </script>
